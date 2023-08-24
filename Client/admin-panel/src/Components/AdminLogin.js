de6b6c8c8a0186f101b4b789e313351a86c83698
Gowtham MA <magowtham7@gmail.com>
2023-08-24 22:59:24 +0530

    implementing otp validation form

--- Client/admin-panel/src/Components/AdminLogin.js
+++ Client/admin-panel/src/Components/AdminLogin.js
@@ -1,12 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../CSS/AdminLogin.css";
 import axios from "axios";
 
 function AdminLogin() {
+  const sendOtpUrl = "http://localhost:5001/admin/sendOtp";
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [loginData, setLoginData] = useState({ usn: "", password: "" });
   const [loginError, setLoginError] = useState({});
+  const [isForgotValidated, setIsForgotValidated] = useState();
+  const [forgotData, setForgotData] = useState({ usn: "" });
+  const [isForgotSetted, setIsForgotSetted] = useState(false);
+  const usnInputRef = useRef(null);
   const navigate = useNavigate();
   const handleLogin = (e) => {
     e.preventDefault();
@@ -23,6 +28,41 @@ function AdminLogin() {
     }
     return errors;
   };
+  const forgotValidater = () => {
+    const error = {};
+    if (!forgotData.usn) {
+      error.usnError = "USN required";
+    }
+    setLoginError(error);
+    return true;
+  };
+  const handleForgot = (e) => {
+    e.preventDefault();
+    setIsForgotSetted(true);
+    setIsForgotValidated(false);
+    setLoginError({});
+    setForgotData({ usn: usnInputRef.current.value });
+  };
+  useEffect(() => {
+    if (isForgotValidated && Object.keys(loginError).keys.length === 0) {
+      (async () => {
+        try {
+          const response = await axios.post(sendOtpUrl, forgotData);
+          console.log(response.data);
+        } catch (err) {
+          console.log(err);
+        } finally {
+          console.log("completed");
+        }
+      })();
+    }
+  }, [isForgotValidated]);
+  useEffect(() => {
+    if (isForgotSetted) {
+      setIsForgotValidated(forgotValidater());
+      setIsForgotSetted(false);
+    }
+  }, [isForgotSetted]);
   useEffect(() => {
     if (isFormSubmitted) {
       setLoginError(loginValidater(loginData));
@@ -49,15 +89,18 @@ function AdminLogin() {
       <div className="admin-login-container">
         <form onSubmit={handleLogin}>
           <label>USN</label>
-          <input type="text" id="usn" placeholder="Universal Serial Number.." />
+          <input
+            ref={usnInputRef}
+            type="text"
+            id="usn"
+            placeholder="Universal Serial Number.."
+          />
           <p>{loginError.usnError}</p>
           <label>Passowrd</label>
           <input type="password" id="password" placeholder="Passowrd.." />
           <p>{loginError.passwordError}</p>
           <input type="submit" value="Submit" />
-          <Link to="/AdminOtp">
-            <p>Forgot password?</p>
-          </Link>
+          <button onClick={handleForgot}>Forgot password?</button>
         </form>
       </div>
     </>
--- Client/admin-panel/src/Components/AdminOtp.js
+++ Client/admin-panel/src/Components/AdminOtp.js
@@ -1,34 +1,64 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
 import "../CSS/AdminOtp.css";
 function AdminOtp() {
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [formData, setFormData] = useState({});
   const [formError, setFormError] = useState({});
   const [isFormValidated, setIsFormValidated] = useState(false);
+  const [disableButton, setDisableButton] = useState(false);
+  const serverMessageRef = useRef(null);
+  const [otpResult, setOtpResult] = useState(false);
+  const baseUrl = "http://localhost:5001/admin/sendOtp";
   const handleOtp = (e) => {
     e.preventDefault();
     setIsFormSubmitted(true);
-    setFormData({});
+    setFormError({});
+    setIsFormValidated(false);
+    serverMessageRef.current.textContent = "";
+    setFormData({ usn: e.target[0].value });
   };
-  const formValidater = (value) => {
+  const formValidater = () => {
     const error = {};
-    if (!value) {
+    if (!formData.usn) {
       error.usnError = "USN required";
     }
-    setFormError(err);
+    setFormError(error);
     return true;
   };
   useEffect(() => {
     if (isFormSubmitted) {
       setIsFormValidated(formValidater());
+      setIsFormSubmitted(false);
     }
   }, [isFormSubmitted]);
+  useEffect(() => {
+    (async () => {
+      if (isFormValidated && Object.keys(formError).length === 0) {
+        try {
+          setDisableButton(true);
+          const response = await axios.post(baseUrl, formData);
+          serverMessageRef.current.textContent = response.data?.message;
+          setOtpResult(response.data?.status);
+        } catch (err) {
+          console.log(err);
+        } finally {
+          setDisableButton(false);
+        }
+      }
+    })();
+  }, [isFormValidated]);
   return (
     <>
       <div className="admin-otp-container">
         <form onSubmit={handleOtp}>
           <input type="text" placeholder="USN..."></input>
-          <button type="submit">Generate OTP</button>
+          <p>{formError.usnError}</p>
+          <p>{disableButton}</p>
+          <button type="submit" disabled={disableButton}>
+            Send OTP
+          </button>
+          <p ref={serverMessageRef}></p>
         </form>
       </div>
     </>
